58fc2fc0393b7452ed8bcfc071f48d7914b8071c
Diego-byte945 <alumno.560757@ies-azarquiel.es>
2025-09-30 15:59:36 +0200

    barra busqueda

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,48 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import header from './assets/header.jpeg';
 import { fetchPokemons } from './services/PokemosService';
 import PokemonCard from './components/PokemonCard';
+import Search from './components/PokemonSearch';
 
 const App = () => {
   const [pokemons, setPokemons] = useState([]);
+  const [filteredPokemons, setFilteredPokemons] = useState([]);
 
-  useEffect(() => {
+   useEffect(() => {
     fetchPokemons()
-      .then(setPokemons)
+      .then(pokemons => {
+        setPokemons(pokemons); 
+        setFilteredPokemons(pokemons);
+      })
       .catch((err) => {
         console.log(err.message);
       });
   }, []);
+
+  const handleSearch = (query) => {
+    if (!query) {
+      setFilteredPokemons(pokemons);
+    } else {
+      setFilteredPokemons(
+        pokemons.filter(pokemon =>
+          pokemon.name.toLowerCase().startsWith(
+            query.toLowerCase())
+        )
+      );
+    }
+  };
     // El return con la foto de los pokemons lo he tenido que hacer con ChatGPT
   return (
     <div className="container shadow p-0">
       <header className="header-bg">
         <img src={header} alt="header" className="w-100"/>
       </header>
+      <div className="row justify-content-center pt-3">
+        <div className="col-10 col-sm-8 col-md-6 col-xl-4">
+          <Search onSearch={handleSearch} />
+        </div>
+      </div>
       <div className="row p-3 justify-content-center">
-        {pokemons.map((pokemon) => (
+        {filteredPokemons.map((pokemon) => (
           <div className="col-md-6 col-lg-4 mb-3">
             <PokemonCard key={pokemon.name} name={pokemon.name} url={pokemon.url}/>
           </div>
